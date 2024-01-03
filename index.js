6feffe60056cdb46d787c2a390bb8e85ac1140e2
justinkunz <justin_kunz@yahoo.com>
2024-01-02 18:17:03 -0600

    added notif sound

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const checkIntervalMs = 10000;
 
 const enableSMS = false;
 const enablePush = true;
+const enableNotifAudio = true;
+const enableTTSAudio = true;
+const overrideMute = true;
 
 // place target names in here, the rest is Disney Magic 🪄 (must exact match mousedining.com)
 const restaurantNames = [
@@ -21,12 +24,16 @@ const PHONE_NUMBERS = process.env.TWILIO_SMS_TO.split("&");
 
 const fs = require("fs");
 const path = require("path");
+const { execSync } = require("child_process");
 const axios = require("axios");
+const text2wav = require("text2wav");
 const client = require("twilio")(
   process.env.TWILIO_SMS_ID,
   process.env.TWILIO_SMS_TOKEN
 );
 const Pushover = require("node-pushover");
+const player = require("play-sound");
+const { v4: uuid } = require("uuid");
 
 const push = new Pushover({
   token: process.env.PUSHOVER_TOKEN,
@@ -115,6 +122,65 @@ const sendReservationSMS = (
   );
 };
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const playNotifAudio = async (name, date, breakfast, brunch, lunch, dinner) => {
+  if (!enableNotifAudio && !enableTTSAudio) return;
+
+  // unmute if muted
+  if (overrideMute) execSync('osascript -e "set volume without output muted"');
+
+  if (enableNotifAudio) {
+    player().play("notif.wav");
+    await sleep(2000); // playback notif sound first
+  }
+
+  if (!enableTTSAudio) return;
+
+  const getTimeblocks = (availability) =>
+    Object.entries(availability)
+      .map(([label, value]) => (["0", 0].includes(value) ? "" : label))
+      .filter((block) => !!block)
+      .reduce(
+        (a, c, i, arr) =>
+          (a += ` ${i === arr.length - 1 ? "and" : i === 0 ? "" : ","} ${c}`),
+        ""
+      )
+      .trim();
+
+  const formattedLongDate = new Intl.DateTimeFormat("en", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  }).format(new Date(`${date}T00:00:00-06:00`));
+
+  const tts = `Dining available at ${name} on ${formattedLongDate} for ${getTimeblocks(
+    {
+      breakfast,
+      brunch,
+      lunch,
+      dinner,
+    }
+  )}`;
+
+  const data = await text2wav(tts);
+
+  const tempNotifFileName = `tts-notif-${uuid()}.wav`;
+  const ttsNotifDir = path.join(__dirname, "tts-notifs");
+  const ttsNotifPath = path.join(ttsNotifDir, tempNotifFileName);
+
+  if (!fs.existsSync(ttsNotifDir)) {
+    fs.mkdirSync(ttsNotifDir);
+  }
+
+  fs.writeFileSync(ttsNotifPath, data);
+  player().play(ttsNotifPath);
+
+  setTimeout(() => {
+    fs.rmSync(ttsNotifPath);
+  }, 15000); // Remove after 15s
+};
+
 const buildReservationUrl = (restCode) =>
   `https://mousedining.com/v1/openings/${startDate}%7C${restCode}%7C${partySize}%7C${stayLengthDays}`;
 
@@ -153,24 +219,20 @@ const checkRestaurant = async (rest) => {
 
     if (opening) {
       recordOpening(rest.name, opening);
-      sendNotif(
+
+      const notifArgs = [
         rest.name,
         opening.Date,
         opening.MealOpenings.Breakfast || 0,
         opening.MealOpenings.Brunch || 0,
         opening.MealOpenings.Lunch || 0,
         opening.MealOpenings.Dinner || 0,
-        rest.reservationUrl
-      );
-      sendReservationSMS(
-        rest.name,
-        opening.Date,
-        opening.MealOpenings.Breakfast || 0,
-        opening.MealOpenings.Brunch || 0,
-        opening.MealOpenings.Lunch || 0,
-        opening.MealOpenings.Dinner || 0,
-        rest.reservationUrl
-      );
+        rest.reservationUrl,
+      ];
+
+      playNotifAudio(...notifArgs);
+      sendNotif(...notifArgs);
+      sendReservationSMS(...notifArgs);
     }
   } catch (err) {
     console.error("Error checking", rest.name);
@@ -178,7 +240,8 @@ const checkRestaurant = async (rest) => {
 };
 
 const setup = async () => {
-  if (!enablePush && !enableSMS) console.warn("No notif targets set");
+  if (!enablePush && !enableSMS && !enableNotifAudio && !enableTTSAudio)
+    console.warn("No notif targets set");
 
   const { data } = await axios.get("https://mousedining.com/v1/restaurants");
 
